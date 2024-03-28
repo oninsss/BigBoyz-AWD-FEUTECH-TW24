1e98ae9a62c7af64f8be51c3b5d7aacd9d6505c4
Mar1154 <noiramyeliab@gmail.com>
2024-03-28 23:49:03 +0800

    Bailey - Timeline and Initial Design 1

---
+++ History/Assets/JS/script.js
@@ -0,0 +1,27 @@
+scroll1 = () => {
+    document.getElementById('_period9').scrollIntoView({behavior: "smooth"});
+}
+scroll2 = () => {
+    document.getElementById('_period8').scrollIntoView({behavior: "smooth"});
+}
+scroll3 = () => {
+    document.getElementById('_period7').scrollIntoView({behavior: "smooth"});
+}
+scroll4 = () => {
+    document.getElementById('_period6').scrollIntoView({behavior: "smooth"});
+}
+scroll5 = () => {
+    document.getElementById('_period5').scrollIntoView({behavior: "smooth"});
+}
+scroll6 = () => {
+    document.getElementById('_period4').scrollIntoView({behavior: "smooth"});
+}
+scroll7 = () => {
+    document.getElementById('_period3').scrollIntoView({behavior: "smooth"});
+}
+scroll8 = () => {
+    document.getElementById('_period2').scrollIntoView({behavior: "smooth"});
+}
+scroll9 = () => {
+    document.getElementById('_period1').scrollIntoView({behavior: "smooth"});
+}
